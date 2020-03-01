27116ac82819cc4b88338a5552dc1006d66d83fc
Fernando <fernandosilvabarreto1@gmail.com>
2020-03-01 15:38:34 -0300

    projeto webScraping

---
+++ WebScraping/MDSNDemo2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace MDSNDemo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.AtualizarButton = new System.Windows.Forms.Button();
+            this.Título = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Postagem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Respostas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Exibições = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewLinkColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Título,
+            this.Postagem,
+            this.Respostas,
+            this.Exibições,
+            this.Link});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(843, 368);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // AtualizarButton
+            // 
+            this.AtualizarButton.Location = new System.Drawing.Point(12, 12);
+            this.AtualizarButton.Name = "AtualizarButton";
+            this.AtualizarButton.Size = new System.Drawing.Size(207, 31);
+            this.AtualizarButton.TabIndex = 1;
+            this.AtualizarButton.Text = "AtualizarLista";
+            this.AtualizarButton.UseVisualStyleBackColor = true;
+            this.AtualizarButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Título
+            // 
+            this.Título.HeaderText = "Título";
+            this.Título.Name = "Título";
+            this.Título.ReadOnly = true;
+            this.Título.Width = 200;
+            // 
+            // Postagem
+            // 
+            this.Postagem.HeaderText = "Postagem";
+            this.Postagem.Name = "Postagem";
+            this.Postagem.ReadOnly = true;
+            this.Postagem.Width = 170;
+            // 
+            // Respostas
+            // 
+            this.Respostas.HeaderText = "Exibições";
+            this.Respostas.Name = "Respostas";
+            this.Respostas.ReadOnly = true;
+            this.Respostas.Width = 90;
+            // 
+            // Exibições
+            // 
+            this.Exibições.HeaderText = "Respostas";
+            this.Exibições.Name = "Exibições";
+            this.Exibições.ReadOnly = true;
+            this.Exibições.Width = 90;
+            // 
+            // Link
+            // 
+            this.Link.HeaderText = "Link";
+            this.Link.Name = "Link";
+            this.Link.ReadOnly = true;
+            this.Link.Width = 250;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 450);
+            this.Controls.Add(this.AtualizarButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MSDN Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button AtualizarButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Título;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Postagem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Respostas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Exibições;
+        private System.Windows.Forms.DataGridViewLinkColumn Link;
+    }
+}
+
---
+++ WebScraping/MDSNDemo2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using HtmlAgilityPack;
+using System.Diagnostics;
+
+namespace MDSNDemo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var wc = new WebClient();
+
+            // faz o download da página
+            //
+            string pagina = wc.DownloadString("https://social.msdn.microsoft.com/forums/pt-br/home?filter=alltypes%sort=fisrtpostdesc");
+   
+            // htmlAgility pack para poder filtrar os dados
+            //necessário fazer dessa maneira pois já existe uma classe nátiva do .Net framkework com mesmo nome
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+
+           
+            htmlDocument.LoadHtml(pagina);
+
+            dataGridView1.Rows.Clear();
+
+            string id,titulo, postagem, exibicao, resposta, link = string.Empty;
+    
+
+            //laço de repetição que adiciona as linhas ao datagridview
+            foreach (HtmlNode node in htmlDocument.GetElementbyId("threadList").ChildNodes)
+            {
+                if (node.Attributes.Count > 0)
+                {   
+                    //cada site é diferente, entao antes de começar a fazer o scrapping é ncessário análisar o site
+                    id = node.Attributes["data-threadid"].Value; 
+                    link = "https://social.msdn.microsoft.com/Forums/pt-BR/" + id;
+                    titulo =  node.Descendants().First(x=>x.Id.Equals("threadTitle_" + id)).InnerText;
+                    postagem = WebUtility.HtmlDecode(node.Descendants().First(x =>x.Attributes["class"] !=null && x.Attributes["class"].Value.Equals("lastpost")).InnerText.Replace("\n","").Replace("  ",""));
+                    exibicao = WebUtility.HtmlDecode(node.Descendants().First(x =>x.Attributes["class"] !=null && x.Attributes["class"].Value.Equals("viewcount")).InnerText);
+                    resposta = WebUtility.HtmlDecode(node.Descendants().First(x =>x.Attributes["class"] !=null && x.Attributes["class"].Value.Equals("replycount")).InnerText);
+
+                    if (!string.IsNullOrEmpty(titulo))
+                        dataGridView1.Rows.Add(titulo, postagem, exibicao, resposta, link);
+
+                }
+            }
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //quando o usuário clicar no link vai abrir uma página no navegador com o link gerado
+            if (e.ColumnIndex == 4)
+                Process.Start(new ProcessStartInfo(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()));
+        }
+    }
+}
